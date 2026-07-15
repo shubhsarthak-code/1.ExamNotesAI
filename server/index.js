89411f53be873878ce058911fa60a34b042e3028
Shubh Sarthak <sarthak132000+github@gmail.com>
2026-07-16 01:17:56 +0530

    Update CORS origin for production deployment

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://one-examnotesai-ytg9.onrender.com",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   }),
