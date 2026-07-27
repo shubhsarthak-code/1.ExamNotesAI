74354d76c980495f5d5bf9f533e803a0b417db66
shubhsarthak-code <sarthak132000+github@gmail.com>
2026-07-27 10:54:53 +0530

    Update Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -32,7 +32,7 @@ function Navbar() {
             transition={{ duration: 1.5 }}
             className='relative z-20 mx-6 mt-6
         rounded-2xl
-        bg-gradient-to-br from-black/90 via-black/80 to-black/90
+        bg-linear-to-br from-black/90 via-black/80 to-black/90
         backdrop-blur-2xl
         border border-white/10
         shadow-[0_22px_55px_rgba(0,0,0,0.75)]
@@ -80,7 +80,7 @@ function Navbar() {
                   animate={{ opacity: 1, y: 10, scale: 1 }}
                   exit={{ opacity: 0, y: -10, scale: 0.95 }}
                   transition={{ duration: 0.2 }}
-                    className='absolute right-[-50px] mt-4 w-64
+                    className='absolute right-12.5 mt-4 w-64
                     rounded-2xl
                     bg-black/90 backdrop-blur-xl
                     border border-white/10
@@ -89,7 +89,7 @@ function Navbar() {
                         <h4 className='font-semibold mb-2'>Buy Credits</h4>
                         <p className='text-sm text-gray-300 mb-4'>Use credits to generate AI notes, diagrams & PDFs.</p>
                         <button onClick={()=>{setShowCredits(false);navigate("/pricing")}} className=' w-full py-2 rounded-lg
-                      bg-gradient-to-br from-white to-gray-200
+                      bg-linear-to-br from-white to-gray-200
                       text-black font-semibold
                       hover:opacity-90'>Buy More Credits</button>
 
